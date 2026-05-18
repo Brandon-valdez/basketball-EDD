22a17b12382ed683fee1f838368f58cd913b9675
Brandon Enox Valdez Linares <brandon.valdez24@itca.edu.sv>
2026-05-17 20:21:34 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/basketball_edd/Basketball_EDD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.basketball_edd;
+
+/**
+ *
+ * @author admin
+ */
+public class Basketball_EDD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
